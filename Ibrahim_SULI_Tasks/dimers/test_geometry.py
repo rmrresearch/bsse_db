3bd14c6406faa304afa8086b0587036325aab325
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-05-28 14:30:47 -0500

    seperate dimers into seperate lists for easier conversion to input script

--- Ibrahim_SULI_Tasks/dimers/test_geometry.py
+++ Ibrahim_SULI_Tasks/dimers/test_geometry.py
@@ -1,9 +1,11 @@
-from utils import geometry
+from utils import geometry, dimer_input
 
 
 def test_geometry():
-    HF_bond_length = 1
-    FF_distance = 4
-    output = geometry(HF_bond_length, FF_distance)
-    expected_output = ["H 0.0 0.0 -1", "F 0.0 0.0 0.0", "F 0.0 0.0 4", "H 0.0 0.0 5"]
-    assert output == expected_output
+  HF_bond_length = 1
+  FF_distance = 4
+  output = geometry(HF_bond_length, FF_distance)
+  expected_output = [['H 0.0 0.0 -1', 'F 0.0 0.0 0.0'],
+                     ['F 0.0 0.0 4', "H 0.0 0.0 5"]
+  ]
+  assert output == expected_output
