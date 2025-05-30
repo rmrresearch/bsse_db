9d13607489c646b92b9c44a77278e2690e8b146e
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-05-30 08:39:36 -0500

    extend geometry to accept coords of second HF monomer

--- Ibrahim_SULI_Tasks/dimers/test_utils.py
+++ Ibrahim_SULI_Tasks/dimers/test_utils.py
@@ -13,13 +13,21 @@ from unittest.mock import patch
 assets_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_assets")
 
 
-def test_get_geometry():
+def test_geometry_FF_distance():
     HF_bond_length = 1
     FF_distance = 4
-    output = get_geometry(HF_bond_length, FF_distance)
+    output = get_geometry(HF_bond_length, FF_distance=FF_distance)
+    expected_output = ["H 0.0 0.0 -1", "F 0.0 0.0 0.0", "F 0.0 0.0 4", "H 0.0 0.0 5"]
+    assert output == expected_output
+
+
+def test_geometry_coords():
+    HF_bond_length = 1
+    F2_coords = [1, 1, 1]
+    output = get_geometry(HF_bond_length, F2_coords=F2_coords)
     expected_output = [
         ["H 0.0 0.0 -1", "F 0.0 0.0 0.0"],
-        ["F 0.0 0.0 4", "H 0.0 0.0 5"],
+        ["F 1.0 1.0 1.0", "H 1.0 1.0 2.0"],
     ]
     assert output == expected_output
 
