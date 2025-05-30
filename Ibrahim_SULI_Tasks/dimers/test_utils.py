24d02a0eb1c9b51d85cccbdd43d261d3bfd9eda1
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-05-30 14:37:37 -0500

    rebase on main and fix formatting

--- Ibrahim_SULI_Tasks/dimers/test_utils.py
+++ Ibrahim_SULI_Tasks/dimers/test_utils.py
@@ -1,61 +1,74 @@
-from utils import get_geometry, dimer_input, monomer_input, read_total_energy, get_optimized_monomer_energy, get_bsse
+from utils import (
+    get_geometry,
+    dimer_input,
+    monomer_input,
+    read_total_energy,
+    get_optimized_monomer_energy,
+    get_bsse,
+)
 import os
 from pytest import approx
 
+
 def test_get_geometry():
-  HF_bond_length = 1
-  FF_distance = 4
-  output = get_geometry(HF_bond_length, FF_distance)
-  expected_output = [['H 0.0 0.0 -1', 'F 0.0 0.0 0.0'],
-                     ['F 0.0 0.0 4', "H 0.0 0.0 5"]
-  ]
-  assert output == expected_output
-def test_dimer_input(tmp_path):
-  geometry = [
-    ['H 0.0 0.0 -1', 
-    'F 0.0 0.0 0.0'],
-    ['F 0.0 0.0 4',
-    "H 0.0 0.0 5"]
-  ]
-  input_path = os.path.join(tmp_path, "input.txt")
-  dimer_input(geometry, input_path)
-  with open(input_path, "r") as f, open("test_assets/input_dimer.txt", "r") as f_expected:
-    output = f.read()
-    expected_output = f_expected.read()
+    HF_bond_length = 1
+    FF_distance = 4
+    output = get_geometry(HF_bond_length, FF_distance)
+    expected_output = [
+        ["H 0.0 0.0 -1", "F 0.0 0.0 0.0"],
+        ["F 0.0 0.0 4", "H 0.0 0.0 5"],
+    ]
     assert output == expected_output
+
+
+def test_dimer_input(tmp_path):
+    geometry = [["H 0.0 0.0 -1", "F 0.0 0.0 0.0"], ["F 0.0 0.0 4", "H 0.0 0.0 5"]]
+    input_path = os.path.join(tmp_path, "input.txt")
+    dimer_input(geometry, input_path)
+    with open(input_path, "r") as f, open(
+        "test_assets/input_dimer.txt", "r"
+    ) as f_expected:
+        output = f.read()
+        expected_output = f_expected.read()
+        assert output == expected_output
+
+
 def test_monomer_input(tmp_path):
-  geometry = [
-    ['H 0.0 0.0 -1', 
-    'F 0.0 0.0 0.0'],
-    ['F 0.0 0.0 4',
-    "H 0.0 0.0 5"]
-  ]
-  monomer_input(geometry, os.path.join(tmp_path,"input_A.txt"), os.path.join(tmp_path,"input_B.txt"))
-  for monomer in ["A", "B"]:
-    with open(os.path.join(tmp_path,f"input_{monomer}.txt"), "r") as f, open(f"test_assets/input_{monomer}_AB.txt", "r") as f_expected:
-      output = f.read()
-      expected_output = f_expected.read()
-      assert output == expected_output
+    geometry = [["H 0.0 0.0 -1", "F 0.0 0.0 0.0"], ["F 0.0 0.0 4", "H 0.0 0.0 5"]]
+    monomer_input(
+        geometry,
+        os.path.join(tmp_path, "input_A.txt"),
+        os.path.join(tmp_path, "input_B.txt"),
+    )
+    for monomer in ["A", "B"]:
+        with open(os.path.join(tmp_path, f"input_{monomer}.txt"), "r") as f, open(
+            f"test_assets/input_{monomer}_AB.txt", "r"
+        ) as f_expected:
+            output = f.read()
+            expected_output = f_expected.read()
+            assert output == expected_output
+
+
 def test_read_total_energy():
-  output = read_total_energy("test_assets/sample_output.txt")
-  assert output == approx(-200.04640230404723, rel=1e-5)
+    output = read_total_energy("test_assets/sample_output.txt")
+    assert output == approx(-200.04640230404723, rel=1e-5)
+
+
 def test_get_optimized_monomer_energy(tmp_path):
-  geometry = [
-    ['H 0.0 0.0 -1',
-    'F 0.0 0.0 0.0'],
-    ['F 0.0 0.0 4',
-    "H 0.0 0.0 5"]
-  ]
-  get_optimized_monomer_energy(geometry, os.path.join(tmp_path,"input_monomer.txt"))
-  with open(os.path.join(tmp_path,f"input_monomer.txt"), "r") as f, open(f"test_assets/input_monomer.txt", "r") as f_expected:
-      output = f.read()
-      expected_output = f_expected.read()
-      assert output == expected_output
+    geometry = [["H 0.0 0.0 -1", "F 0.0 0.0 0.0"], ["F 0.0 0.0 4", "H 0.0 0.0 5"]]
+    get_optimized_monomer_energy(geometry, os.path.join(tmp_path, "input_monomer.txt"))
+    with open(os.path.join(tmp_path, f"input_monomer.txt"), "r") as f, open(
+        f"test_assets/input_monomer.txt", "r"
+    ) as f_expected:
+        output = f.read()
+        expected_output = f_expected.read()
+        assert output == expected_output
+
+
 def test_calculate_bsse(tmp_path):
-  geometry = [['H 0.0 0.0 -0.92', 'F 0.0 0.0 0.0'],
-               ['F 0.0 0.0 2', 'H 0.0 0.0 2.92']]
-  output = get_bsse(geometry, tmp_path)
-  print(output)
-  assert output["BSSE_A"] == approx(0.0004733027579248983, rel=1e-5)
-  assert output["BSSE_B"] == approx(0.0004733027579248983, rel=1e-5)
-  assert output["Delta_E_AB_AB"] == approx(0.023323579176050657, rel=1e-5)
+    geometry = [["H 0.0 0.0 -0.92", "F 0.0 0.0 0.0"], ["F 0.0 0.0 2", "H 0.0 0.0 2.92"]]
+    output = get_bsse(geometry, tmp_path)
+    print(output)
+    assert output["BSSE_A"] == approx(0.0004733027579248983, rel=1e-5)
+    assert output["BSSE_B"] == approx(0.0004733027579248983, rel=1e-5)
+    assert output["Delta_E_AB_AB"] == approx(0.023323579176050657, rel=1e-5)
--- Ibrahim_SULI_Tasks/dimers/utils.py
+++ Ibrahim_SULI_Tasks/dimers/utils.py
@@ -1,94 +1,106 @@
 import subprocess
 import os
+
+
 def get_geometry(HF_bond_length, FF_distance):
-  return [
-    [f'H 0.0 0.0 -{HF_bond_length}', 'F 0.0 0.0 0.0'],
-    [f'F 0.0 0.0 {FF_distance}', f"H 0.0 0.0 {FF_distance + HF_bond_length}"]
-  ]
+    return [
+        [f"H 0.0 0.0 -{HF_bond_length}", "F 0.0 0.0 0.0"],
+        [f"F 0.0 0.0 {FF_distance}", f"H 0.0 0.0 {FF_distance + HF_bond_length}"],
+    ]
+
+
 def dimer_input(geometry, input_file_path):
-  with open(input_file_path, "w") as f:
-    f.write("memory 600 mb\n")
-    f.write("\n")
-    f.write("molecule HF {\n")
-    for i, molecule in enumerate(geometry):
-      for line in molecule:
-        f.write(f"  {line}\n")
-      if i != len(geometry) - 1:
-        f.write("  --\n")
-    f.write("}\n")
-    f.write("\n")
-    f.write("set basis aug-cc-pVDZ\n")
-    f.write("energy('ccsd(t)')\n")
+    with open(input_file_path, "w") as f:
+        f.write("memory 600 mb\n")
+        f.write("\n")
+        f.write("molecule HF {\n")
+        for i, molecule in enumerate(geometry):
+            for line in molecule:
+                f.write(f"  {line}\n")
+            if i != len(geometry) - 1:
+                f.write("  --\n")
+        f.write("}\n")
+        f.write("\n")
+        f.write("set basis aug-cc-pVDZ\n")
+        f.write("energy('ccsd(t)')\n")
+
+
 def monomer_input(geometry, input_A, input_B):
-  with open(input_A, "w") as f:
-    f.write("memory 600 mb\n")
-    f.write("\n")
-    f.write("molecule HF {\n")
-    for i, molecule in enumerate(geometry):
-      for line in molecule:
-        f.write(f"  {line}\n" if i == 0 else f"  @{line}\n")
-      if i != len(geometry) - 1:
-        f.write("  --\n")
-    f.write("}\n")
-    f.write("\n")
-    f.write("set basis aug-cc-pVDZ\n")
-    f.write("energy('ccsd(t)')\n")
-  with open(input_B, "w") as f:
-    f.write("memory 600 mb\n")
-    f.write("\n")
-    f.write("molecule HF {\n")
-    for i, molecule in enumerate(geometry):
-      for line in molecule:
-        f.write(f"  {line}\n" if i == 1 else f"  @{line}\n")
-      if i != len(geometry) - 1:
-        f.write("  --\n")
-    f.write("}\n")
-    f.write("\n")
-    f.write("set basis aug-cc-pVDZ\n")
-    f.write("energy('ccsd(t)')\n")
+    with open(input_A, "w") as f:
+        f.write("memory 600 mb\n")
+        f.write("\n")
+        f.write("molecule HF {\n")
+        for i, molecule in enumerate(geometry):
+            for line in molecule:
+                f.write(f"  {line}\n" if i == 0 else f"  @{line}\n")
+            if i != len(geometry) - 1:
+                f.write("  --\n")
+        f.write("}\n")
+        f.write("\n")
+        f.write("set basis aug-cc-pVDZ\n")
+        f.write("energy('ccsd(t)')\n")
+    with open(input_B, "w") as f:
+        f.write("memory 600 mb\n")
+        f.write("\n")
+        f.write("molecule HF {\n")
+        for i, molecule in enumerate(geometry):
+            for line in molecule:
+                f.write(f"  {line}\n" if i == 1 else f"  @{line}\n")
+            if i != len(geometry) - 1:
+                f.write("  --\n")
+        f.write("}\n")
+        f.write("\n")
+        f.write("set basis aug-cc-pVDZ\n")
+        f.write("energy('ccsd(t)')\n")
+
+
 def read_total_energy(file_path):
-  with open(file_path, "r") as f:
-    for line in f:
-      if "Total Energy =" in line:
-        return float(line.split()[-1])
-    raise ValueError(f"Total energy not found in {file_path}")
+    with open(file_path, "r") as f:
+        for line in f:
+            if "Total Energy =" in line:
+                return float(line.split()[-1])
+        raise ValueError(f"Total energy not found in {file_path}")
+
+
 def get_optimized_monomer_energy(geometry, input_file_path):
-  monomer = geometry[0]
-  with open(input_file_path, "w") as f:
-    f.write("memory 600 mb\n")
-    f.write("\n")
-    f.write("molecule HF {\n")
-    for line in monomer:
-      f.write(f"  {line}\n")
-    f.write("}\n")
-    f.write("\n")
-    f.write("set basis aug-cc-pVDZ\n")
-    f.write("energy('ccsd(t)')\n")
-def get_bsse(geometry, scripts_dir =  "scripts") -> dict:
-  """
-  Returns a dictonary of the form
-  {
-    "Delta_E_AB_AB": float,
-    "BSSE_A": float,
-    "BSSE_B": float
-  }"""
-  prev_dir = os.getcwd()
-  os.makedirs(scripts_dir, exist_ok=True)
-  os.chdir(scripts_dir)
-  dimer_input(geometry, "input_dimer.txt")
-  monomer_input(geometry, "input_A_AB.txt", "input_B_AB.txt")
-  get_optimized_monomer_energy(geometry, "input_monomer.txt")
-  subprocess.run(["psi4", "input_dimer.txt", "output_dimer.txt"])
-  subprocess.run(["psi4", "input_A_AB.txt", "output_A_AB.txt"])
-  subprocess.run(["psi4", "input_B_AB.txt", "output_B_AB.txt"])
-  subprocess.run(["psi4", "input_monomer.txt", "output_monomer.txt"])
-  total_energy = read_total_energy("output_dimer.txt")
-  E_A_AB = read_total_energy("output_A_AB.txt")
-  E_B_AB = read_total_energy("output_B_AB.txt")
-  E_monomer = read_total_energy("output_monomer.txt")
-  os.chdir(prev_dir)
-  return {
-    "Delta_E_AB_AB": total_energy - E_A_AB - E_B_AB,
-    "BSSE_A": E_monomer - E_A_AB,
-    "BSSE_B": E_monomer - E_B_AB
-  }
+    monomer = geometry[0]
+    with open(input_file_path, "w") as f:
+        f.write("memory 600 mb\n")
+        f.write("\n")
+        f.write("molecule HF {\n")
+        for line in monomer:
+            f.write(f"  {line}\n")
+        f.write("}\n")
+        f.write("\n")
+        f.write("set basis aug-cc-pVDZ\n")
+        f.write("energy('ccsd(t)')\n")
+
+
+def get_bsse(geometry, scripts_dir="scripts") -> dict:
+    """
+    Returns a dictonary of the form
+    {
+      "Delta_E_AB_AB": float,
+      "BSSE_A": float,
+      "BSSE_B": float
+    }"""
+    prev_dir = os.getcwd()
+    os.makedirs(scripts_dir, exist_ok=True)
+    os.chdir(scripts_dir)
+    dimer_input(geometry, "input_dimer.txt")
+    monomer_input(geometry, "input_A_AB.txt", "input_B_AB.txt")
+    get_optimized_monomer_energy(geometry, "input_monomer.txt")
+    subprocess.run(["psi4", "input_dimer.txt", "output_dimer.txt"])
+    subprocess.run(["psi4", "input_A_AB.txt", "output_A_AB.txt"])
+    subprocess.run(["psi4", "input_B_AB.txt", "output_B_AB.txt"])
+    subprocess.run(["psi4", "input_monomer.txt", "output_monomer.txt"])
+    total_energy = read_total_energy("output_dimer.txt")
+    E_A_AB = read_total_energy("output_A_AB.txt")
+    E_B_AB = read_total_energy("output_B_AB.txt")
+    E_monomer = read_total_energy("output_monomer.txt")
+    os.chdir(prev_dir)
+    return {
+        "Delta_E_AB_AB": total_energy - E_A_AB - E_B_AB,
+        "BSSE_A": E_monomer - E_A_AB,
+        "BSSE_B": E_monomer - E_B_AB,
+    }
