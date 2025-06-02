28bcabe0052419617d2069f6befca1a3e816330a
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-06-02 15:20:03 -0500

    open image when env variable set

--- Ibrahim_SULI_Tasks/dimers/test_utils.py
+++ Ibrahim_SULI_Tasks/dimers/test_utils.py
@@ -96,5 +96,5 @@ def test_get_bsse(tmp_path):
 def test_make_diagram(tmp_path):
     geometry = [["H 0.0 0.0 -0.92", "F 0.0 0.0 0.0"], ["F 0.0 0.0 2", "H 0.0 0.0 2.92"]]
     make_diagram(geometry, os.path.join(tmp_path, "diagram.html"))
-    if os.environ.get("OPEN_IMAGES") == 1:
+    if os.environ.get("OPEN_IMAGES"):
         subprocess.run(["open", os.path.join(tmp_path, "diagram.html")])
