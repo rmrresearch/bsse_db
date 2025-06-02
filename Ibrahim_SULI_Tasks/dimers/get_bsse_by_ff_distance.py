9db202f801bb55bda9ea0cd67440fba61602ed2b
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-06-02 11:46:00 -0500

    reformat

--- Ibrahim_SULI_Tasks/dimers/get_bsse_by_ff_distance.py
+++ Ibrahim_SULI_Tasks/dimers/get_bsse_by_ff_distance.py
@@ -1,18 +1,17 @@
 from utils import get_bsse, get_geometry
 import os
+
 if __name__ == "__main__":
-  BSSE_As = []
-  BSSE_Bs = []
-  FF_distances =[3,4,5,6]
-  for FF_distance in FF_distances:
-    geometry = get_geometry(.924, FF_distance)
-    output = get_bsse(geometry, f"FF_distance_{FF_distance}")
-    BSSE_As.append(output["BSSE_A"])
-    BSSE_Bs.append(output["BSSE_B"])
-  os.makedirs("data", exist_ok=True)
-  with open(os.path.join("data", "BSSE_by_FF_distance.csv"), "w") as f:
-    f.write("FF_distance,BSSE_A,BSSE_B\n")
-    for BSSE_A, BSSE_B, FF_distance in zip(BSSE_As, BSSE_Bs, FF_distances):
-      f.write(f"{FF_distance},{BSSE_A},{BSSE_B}\n")
-    
-    
+    BSSE_As = []
+    BSSE_Bs = []
+    FF_distances = [3, 4, 5, 6]
+    for FF_distance in FF_distances:
+        geometry = get_geometry(0.924, FF_distance)
+        output = get_bsse(geometry, f"FF_distance_{FF_distance}")
+        BSSE_As.append(output["BSSE_A"])
+        BSSE_Bs.append(output["BSSE_B"])
+    os.makedirs("data", exist_ok=True)
+    with open(os.path.join("data", "BSSE_by_FF_distance.csv"), "w") as f:
+        f.write("FF_distance,BSSE_A,BSSE_B\n")
+        for BSSE_A, BSSE_B, FF_distance in zip(BSSE_As, BSSE_Bs, FF_distances):
+            f.write(f"{FF_distance},{BSSE_A},{BSSE_B}\n")
