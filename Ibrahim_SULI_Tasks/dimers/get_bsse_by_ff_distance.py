4a233813a4999db30fbc677de3d34f61496d25a8
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-06-02 14:20:40 -0500

    add data for each coordinate

--- Ibrahim_SULI_Tasks/dimers/get_bsse_by_ff_distance.py
+++ Ibrahim_SULI_Tasks/dimers/get_bsse_by_ff_distance.py
@@ -1,17 +1,19 @@
 from utils import get_bsse, get_geometry
+import itertools
 import os
 
 if __name__ == "__main__":
     BSSE_As = []
     BSSE_Bs = []
-    FF_distances = [3, 5, 6]
-    for FF_distance in FF_distances:
-        geometry = get_geometry(0.924, FF_distance)
-        output = get_bsse(geometry, f"FF_distance_{FF_distance}")
+    coordinate_list = []
+    for coords in itertools.product([1, 2, 3, 4], repeat=3):
+        geometry = get_geometry(0.924, F2_coords=coords)
+        output = get_bsse(geometry, f"FF_distance_{'_'.join(map(str, coords))}")
         BSSE_As.append(output["BSSE_A"])
         BSSE_Bs.append(output["BSSE_B"])
+        coordinate_list.append(coords)
     os.makedirs("data", exist_ok=True)
     with open(os.path.join("data", "BSSE_by_FF_distance.csv"), "w") as f:
-        f.write("FF_distance,BSSE_A,BSSE_B\n")
-        for BSSE_A, BSSE_B, FF_distance in zip(BSSE_As, BSSE_Bs, FF_distances):
-            f.write(f"{FF_distance},{BSSE_A},{BSSE_B}\n")
+        f.write("coordinates,BSSE_A,BSSE_B\n")
+        for coord, BSSE_A, BSSE_B in zip(coordinate_list, BSSE_As, BSSE_Bs):
+            f.write(f"{coord},{BSSE_A},{BSSE_B}\n")
