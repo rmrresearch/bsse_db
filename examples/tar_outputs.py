03028786527a55e96867527055753641ea9d0855
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-06-05 11:04:54 -0500

    run black

--- examples/tar_outputs.py
+++ examples/tar_outputs.py
@@ -1,17 +1,16 @@
 import tarfile
 import glob
 import os
-parent_folder = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))
-)
+
+parent_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 paths = [
     os.path.join(parent_folder, "examples", "data", "nwchem"),
     os.path.join(parent_folder, "examples", "data", "psi4"),
 ]
 for path in paths:
-  files = glob.glob("output_*.txt", root_dir=path)
-  with tarfile.open(os.path.join(path, "outputs.tar"), "w") as tar:
-      for file in [os.path.join(path, file) for file in files]:
-          tar.add(file)
-          os.remove(file)
+    files = glob.glob("output_*.txt", root_dir=path)
+    with tarfile.open(os.path.join(path, "outputs.tar"), "w") as tar:
+        for file in [os.path.join(path, file) for file in files]:
+            tar.add(file)
+            os.remove(file)
--- src/bsse_calculator/generate_inputs.py
+++ src/bsse_calculator/generate_inputs.py
@@ -1,4 +1,6 @@
 import os
+
+
 class GenerateInputs:
     def __init__(self):
         pass
