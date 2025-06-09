11832ff99c6f29fc47cb4947b2af6d4226c19089
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-06-09 14:41:48 -0500

    clean up

--- src/bsse_calculator/generate_dimer_geometry.py
+++ src/bsse_calculator/generate_dimer_geometry.py
@@ -1,10 +1,9 @@
-
 def generate_dimer_geometry(monomer_geometry, distance):
-  """
-  Accepts a monomer and creates a dimer with the first atom in the geometry separated by distance
-  """
-  second_monomer = []
-  for line in monomer_geometry:
-    atom, x, y, z = line.split()
-    second_monomer.append(f"{atom} {x} {y} {float(z) + distance}")
-  return [monomer_geometry, second_monomer]
+    """
+    Accepts a monomer and creates a dimer with the first atom in the geometry separated by distance
+    """
+    second_monomer = []
+    for line in monomer_geometry:
+        atom, x, y, z = line.split()
+        second_monomer.append(f"{atom} {x} {y} {float(z) + distance}")
+    return [monomer_geometry, second_monomer]
